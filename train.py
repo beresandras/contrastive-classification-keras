c61f9b8080dc03d92fd754ae9ce65ae87db3e506
beresandras <30826103+beresandras@users.noreply.github.com>
2021-09-05 21:05:00 +0200

    improved DINO hyperparameters

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ hyperparams = {
     SimCLR: {"temperature": 0.1},
     BarlowTwins: {"redundancy_reduction_weight": 10.0},
     MoCo: {"momentum_coeff": 0.99, "temperature": 0.1, "queue_size": 10000},
-    DINO: {"momentum_coeff": 0.8, "temperature": 0.1, "sharpening": 0.25},
+    DINO: {"momentum_coeff": 0.9, "temperature": 0.1, "sharpening": 0.5},
 }
 
 # load STL10 dataset
